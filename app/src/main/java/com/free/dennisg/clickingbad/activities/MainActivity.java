12770764c07b460da1e7bc14c2060b8ed9e67545
Dennis Gimbergsson <dennisgimbergsson@live.se>
2017-09-24 18:56:16 +0200

    inital commit

---
+++ app/src/main/java/com/free/dennisg/clickingbad/activities/MainActivity.java
@@ -0,0 +1,102 @@
+package com.free.dennisg.clickingbad.activities;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.free.dennisg.clickingbad.fragments.DistributionFragment;
+import com.free.dennisg.clickingbad.fragments.ManufacturingFragment;
+import com.free.dennisg.clickingbad.R;
+import com.free.dennisg.clickingbad.adapters.MainViewPagerAdapter;
+import com.free.dennisg.clickingbad.fragments.StatsFragment;
+import com.free.dennisg.clickingbad.fragments.StoreFragment;
+import com.squareup.otto.Bus;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
+    @BindView(R.id.main_view_pager)
+    ViewPager viewPager;
+
+    private Unbinder unbinder;
+    private Bus eventBus;
+    private FragmentManager fragmentManager;
+    private MenuItem prevMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Bind butterknife to activity layout components
+        unbinder = ButterKnife.bind(this);
+
+        // Register activity to be able to receive events from different classes
+        eventBus = new Bus();
+        eventBus.register(this);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_manufacturing:
+                        setCurrentFragment(0);
+                        return true;
+                    case R.id.navigation_distribution:
+                        setCurrentFragment(1);
+                        return true;
+                    case R.id.navigation_store:
+                        setCurrentFragment(2);
+                        return true;
+                }
+                return false;
+            }
+
+            private void setCurrentFragment(int itemId) {
+                viewPager.setCurrentItem(itemId);
+            }
+        });
+
+        viewPager.setOffscreenPageLimit(4);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
+        setUpFragments(viewPager);
+    }
+
+    public void setUpFragments(ViewPager viewPager) {
+        MainViewPagerAdapter adapter = new MainViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ManufacturingFragment(), getResources().getString(R.string.title_manufacturing));
+        adapter.addFragment(new DistributionFragment(), getResources().getString(R.string.title_distribution));
+        adapter.addFragment(new StoreFragment(), getResources().getString(R.string.title_store));
+        viewPager.setAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/com/free/dennisg/clickingbad/adapters/MainViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.free.dennisg.clickingbad.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dennis Gimbergsson on 2017-09-24.
+ */
+
+public class MainViewPagerAdapter  extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public MainViewPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitleList.get(position);
+    }
+}
