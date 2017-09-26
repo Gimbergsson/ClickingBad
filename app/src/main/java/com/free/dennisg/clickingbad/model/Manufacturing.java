a1bd9dc7c5cf4ef4eadcf8a17815031ba2d0aa93
Dennis Gimbergsson <dennisgimbergsson@live.se>
2017-09-26 23:15:00 +0200

    init

--- app/src/main/java/com/free/dennisg/clickingbad/model/Manufacturing.java
+++ app/src/main/java/com/free/dennisg/clickingbad/model/Manufacturing.java
@@ -7,9 +7,23 @@ package com.free.dennisg.clickingbad.model;
 public class Manufacturing {
 
     private int itemId = 0;
+    private String name;
+    private String description;
+    private int cost;
+    private int risk;
+    private int purity;
+    private int cook;
 
-    public Manufacturing(int itemId) {
+
+
+    public Manufacturing(int itemId, String name, String description, int cost, int risk, int purity, int cook) {
         this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.risk = risk;
+        this.purity = purity;
+        this.cook = cook;
     }
 
     public int getItemId() {
@@ -18,5 +32,53 @@ public class Manufacturing {
 
     public void setItemId(int itemId) {
         this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getRisk() {
+        return risk;
+    }
+
+    public void setRisk(int risk) {
+        this.risk = risk;
+    }
+
+    public int getPurity() {
+        return purity;
+    }
+
+    public void setPurity(int purity) {
+        this.purity = purity;
+    }
+
+    public int getCook() {
+        return cook;
+    }
+
+    public void setCook(int cook) {
+        this.cook = cook;
     }
 }
